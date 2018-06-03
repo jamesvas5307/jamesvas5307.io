a82c628c18f70015bb30a187449ba09be9cd1585
James Vasquez <jamesvas5307@live.com>
2018-06-03 18:04:13 -0400

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+declare var require: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
+
+
+// An alternative to ng-repeat
